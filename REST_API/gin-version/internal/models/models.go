cbf6dd101e43724e660e818b648e386cb4f5c917
Anastasia <milessn@MacBook-Air-Anastasia-2.local>
2026-09-17 11:46:48 +0300

    feat: add Order, OrderItem models and DTOs

--- REST_API/gin-version/internal/models/models.go
+++ REST_API/gin-version/internal/models/models.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type User struct {
 	ID   int    `json:"id"`
 	Name string `json:"name" binding:"required"`
@@ -8,4 +10,31 @@ type User struct {
 type UserUpdate struct {
 	Name *string `json:"name"`
 	Age  *int    `json:"age"`
+}
+
+type Order struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"user_id" binding:"required"`
+	Status    *string   `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+}
+type OrderItem struct {
+	ID          int    `json:"id"`
+	OrderId     int    `json:"order_id" binding:"required"`
+	ProductName string `json:"product_name" binding:"required"`
+	Price       *int   `json:"price" binding:"required,gt=-1"`
+	Quantity    int    `json:"quantity" binding:"required,gt=0"`
+}
+type CreateOrderItemRequest struct {
+	ProductName string `json:"product_name" binding:"required"`
+	Price       *int   `json:"price" binding:"required,gt=-1"`
+	Quantity    int    `json:"quantity" binding:"required,gt=0"`
+}
+type CreateOrderRequest struct {
+	UserID     int                      `json:"user_id" binding:"required"`
+	Status     *string                  `json:"status"`
+	OrderItems []CreateOrderItemRequest `json:"order_items" binding:"required,min=1,dive"`
+}
+type orderResponse struct {
+	Order
 }
