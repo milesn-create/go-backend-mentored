1e165a36976e2b1288f5a31e47352812d5a4a109
Anastasia <milessn@MacBook-Air-Anastasia-2.local>
2026-09-18 13:24:41 +0300

    refactor: rename service.go to user_service.go

--- REST_API/gin-version/internal/service/user_service.go
+++ REST_API/gin-version/internal/service/user_service.go
@@ -4,15 +4,10 @@ import (
 	"errors"
 	"rest-api-gin/internal/models"
 	"rest-api-gin/internal/repository"
-
-	"github.com/jackc/pgx/v5/pgconn"
 )
 
 type UserService struct {
 	repo repository.UserRepository
-}
-type OrderService struct {
-	repo repository.OrderRepository
 }
 
 func NewUserService(r repository.UserRepository) *UserService {
@@ -21,24 +16,31 @@ func NewUserService(r repository.UserRepository) *UserService {
 	}
 
 }
-func NewOrderService(r repository.OrderRepository) *OrderService {
-	return &OrderService{repo: r}
-}
 
 var ErrNameAlreadyExists = errors.New("this name already exists")
 var ErrInvalidAge = errors.New("age must be > 0")
 
-func (s *OrderService) CreateOrder(order models.CreateOrderRequest) (models.OrderResponse, error) {
-	var pgErr *pgconn.PgError
-	resultResponse, err := s.repo.CreateOrder(order)
+func (s *UserService) FindByID(id int) (models.User, error) {
+	user, exists, err := s.repo.FindByID(id)
 	if err != nil {
-		if errors.As(err, &pgErr) && pgErr.Code == "23503" {
-			return models.OrderResponse{}, repository.ErrUserNotFound
-		}
-		return models.OrderResponse{}, err
+		return models.User{}, err
+	}
+	if !exists {
+		return models.User{}, repository.ErrUserNotFound
+	}
+	return user, nil
+}
+
+func (s *UserService) CreateUser(user models.User) (models.User, error) {
+	u, exists, err := s.repo.FindByName(user.Name)
+	if err != nil {
+		return models.User{}, err
+	}
+	if exists {
+		return u, errors.New("user already exists")
 
 	}
-	return resultResponse, nil
+	return s.repo.Create(user)
 
 }
 func (s *UserService) UpdateUser(id int, fields models.UserUpdate) (models.User, error) {
@@ -71,34 +73,12 @@ func (s *UserService) UpdateUser(id int, fields models.UserUpdate) (models.User,
 	return updateUser, nil
 
 }
+
 func (s *UserService) DeleteUser(id int) (models.User, error) {
 	user, err := s.repo.DeleteUser(id)
 	if err != nil {
 		return models.User{}, err
 	}
 	return user, nil
-
-}
-func (s *UserService) FindByID(id int) (models.User, error) {
-	user, exists, err := s.repo.FindByID(id)
-	if err != nil {
-		return models.User{}, err
-	}
-	if !exists {
-		return models.User{}, repository.ErrUserNotFound
-	}
-	return user, nil
-}
-
-func (s *UserService) CreateUser(user models.User) (models.User, error) {
-	u, exists, err := s.repo.FindByName(user.Name)
-	if err != nil {
-		return models.User{}, err
-	}
-	if exists {
-		return u, errors.New("user already exists")
-
-	}
-	return s.repo.Create(user)
 
 }
