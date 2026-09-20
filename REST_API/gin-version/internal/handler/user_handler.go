a6e512e96ecd8270a040b94decadb7c0ac002292
Anastasia <milessn@MacBook-Air-Anastasia-2.local>
2026-09-20 20:45:15 +0300

    refactor: split handler.go into user_handler.go

--- REST_API/gin-version/internal/handler/user_handler.go
+++ REST_API/gin-version/internal/handler/user_handler.go
@@ -40,23 +40,7 @@ func CreateUserHandler(c *gin.Context) {
 
 	c.String(200, "Данные нового пользователя: ID - %d Имя - %s, возраст - %d\n", createdUser.ID, createdUser.Name, createdUser.Age)
 }
-func ByeHandler(c *gin.Context) {
-	c.String(200, "Bye(((\n")
-}
-func SumHandler(c *gin.Context) {
-	a, err := strconv.Atoi(c.Query("a"))
-	if err != nil {
-		c.String(http.StatusBadRequest, "a - not an integer\n")
-		return
-	}
-	b, err := strconv.Atoi(c.Query("b"))
-	if err != nil {
-		c.String(http.StatusBadRequest, "b - not an integer\n")
-		return
-	}
-	sum := a + b
-	c.String(200, "Сумма = %d\n", sum)
-}
+
 func IdHandler(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
@@ -113,7 +97,7 @@ func UpdateHandler(c *gin.Context) {
 func DeleteHandler(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.String(http.StatusBadRequest, "id - not an integer")
+		c.String(http.StatusBadRequest, err.Error())
 		return
 
 	}
@@ -128,25 +112,6 @@ func DeleteHandler(c *gin.Context) {
 
 	}
 
-	c.String(http.StatusOK, "Пользователь : (ID - %d, имя - %s, возраст - %d) - успешно удален!", user.ID, user.Name, user.Age)
-
-}
-func LoggerMiddleware(c *gin.Context) {
-	fmt.Println("Пришел запрос: ", c.Request.Method, c.Request.URL.Path)
-}
-func AuthMiddleware(c *gin.Context) {
-	token := c.GetHeader("Authorization")
-	if token == "" {
-		c.String(http.StatusUnauthorized, "Unauthorized\n")
-		c.Abort()
-		return
-
-	}
-	c.Next()
-
-}
-
-func HelloHandler(c *gin.Context) {
-	c.String(200, "Hello, baby)))\n")
+	c.String(http.StatusOK, "Пользователь : (ID - %d, имя - %s, возраст - %d) успешно удален!", user.ID, user.Name, user.Age)
 
 }
